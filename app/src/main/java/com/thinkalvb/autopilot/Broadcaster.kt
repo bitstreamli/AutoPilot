abca5447adedeea294f2a7fd6b1a050ba16f344b
ThinkalVB <thinkalvb@gmail.com>
2021-04-05 00:02:57 +0530

    Timed execution and frame rates

--- app/src/main/java/com/thinkalvb/autopilot/Broadcaster.kt
+++ app/src/main/java/com/thinkalvb/autopilot/Broadcaster.kt
@@ -12,47 +12,60 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 private const val TAG = "Pilot_Broadcaster"
 private const val DATA_BROADCAST_INTERVAL = 100
+private const val FRAME_BROADCAST_INTERVAL = 40
+private const val PROBE_INTERVAL = 5000
 private const val KEEP_ALIVE_TIMEOUT = 15000
+private const val RECEIVE_TIMEOUT = 5
 
 class Broadcaster (private val mDestinationIP: InetAddress, private val mDestinationPort: Int ) : Runnable {
     private lateinit var mUdpSocket: DatagramSocket
+
     private var mLastDataBroadcastTime: Long = 0
-    private var mLastKeepAlive: Long = 0
+    private var mLastFrameBroadcastTime: Long = 0
+    private var mLastReceiveTime: Long = 0
+    private var mLastProbeSendTime: Long = 0
+
+    private var mRequiresDataBroadcast: Boolean = false
+    private var mRequiresFrameBroadcast: Boolean = false
 
     override fun run() {
+
         try {
             mUdpSocket = DatagramSocket()
-            mUdpSocket.soTimeout = 10
+            mUdpSocket.soTimeout = RECEIVE_TIMEOUT
             Log.d(TAG, "Broadcaster Started on port ${mUdpSocket.localPort}")
-            while(!Thread.currentThread().isInterrupted)
-            {
-                val currentTime = System.currentTimeMillis()
-                val timeTillLastDataBroadcast = currentTime - mLastDataBroadcastTime
-                val timeTillLastKeepAlive = currentTime - mLastKeepAlive
-
-                if (timeTillLastDataBroadcast > DATA_BROADCAST_INTERVAL) {
-                    broadcastData()
-                    mLastDataBroadcastTime = currentTime
-                }
-                broadcastFrame()
-
-                val dataBuffer = ByteArray(128)
-                val incomingPacket = DatagramPacket(dataBuffer, dataBuffer.size)
-                needToBroadcast = timeTillLastKeepAlive < KEEP_ALIVE_TIMEOUT
-                try{
-                    mUdpSocket.receive(incomingPacket)
-                    mLastKeepAlive = currentTime
-                    processData(dataBuffer, incomingPacket.length)
-                } catch (e: Exception) {}
-            }
         } catch (e: SocketException) {
-            Log.d(TAG, "Socket Exception $e")
+            //Log.d(TAG, "Socket Exception $e")
         } catch (e: IOException) {
-            Log.d(TAG,"IO Exception $e")
+            //Log.d(TAG,"IO Exception $e")
         } catch (e: Exception) {
-            Log.d(TAG,"Exception $e")
+            //Log.d(TAG,"Exception $e")
+        }
+
+        while(!Thread.currentThread().isInterrupted)
+        {
+            updateFlags()
+            try{
+                if(mRequiresDataBroadcast) broadcastData()
+                if(mRequiresFrameBroadcast) broadcastFrame()
+                receiveData()
+            } catch (e: SocketException) {
+                //Log.d(TAG, "Socket Exception $e")
+            } catch (e: IOException) {
+                //Log.d(TAG,"IO Exception $e")
+            } catch (e: Exception) {
+                //Log.d(TAG,"Exception $e")
+            }
         }
         Log.d(TAG, "Broadcaster Stopped")
+    }
+
+    private fun receiveData() {
+        val dataBuffer = ByteArray(128)
+        val incomingPacket = DatagramPacket(dataBuffer, dataBuffer.size)
+        mUdpSocket.receive(incomingPacket)
+        mLastReceiveTime = System.currentTimeMillis()
+        processData(dataBuffer, incomingPacket.length)
     }
 
     private fun processData(dataBuffer: ByteArray, packetSize: Int) {
@@ -61,6 +74,7 @@ class Broadcaster (private val mDestinationIP: InetAddress, private val mDestina
     }
 
     private fun broadcastData() {
+        mLastDataBroadcastTime = System.currentTimeMillis()
         if(mDataQueue.isNotEmpty()){
             var dataBuffer = ByteArray(0)
             while (mDataQueue.isNotEmpty()) dataBuffer += mDataQueue.poll()
@@ -70,11 +84,36 @@ class Broadcaster (private val mDestinationIP: InetAddress, private val mDestina
     }
 
     private fun broadcastFrame() {
+        mLastFrameBroadcastTime = System.currentTimeMillis()
         if(mFrameQueue.isNotEmpty()){
             val dataBuffer: ByteArray = mFrameQueue.poll()
             val packet = DatagramPacket(dataBuffer, dataBuffer.size, mDestinationIP, mDestinationPort)
             mUdpSocket.send(packet)
         }
+    }
+
+    private fun updateFlags() {
+        val currentTime = System.currentTimeMillis()
+        val timeTillLastDataBroadcast = currentTime - mLastDataBroadcastTime
+        mRequiresDataBroadcast = timeTillLastDataBroadcast > DATA_BROADCAST_INTERVAL
+
+        val timeTillLastFrameBroadcast = currentTime - mLastFrameBroadcastTime
+        mRequiresFrameBroadcast = timeTillLastFrameBroadcast > FRAME_BROADCAST_INTERVAL
+
+        val timeTillLastReceive = currentTime - mLastReceiveTime
+        needToBroadcast = timeTillLastReceive < KEEP_ALIVE_TIMEOUT
+
+        if(!needToBroadcast) {
+            val timeTillLastProbeSend = currentTime - mLastProbeSendTime
+            if (timeTillLastProbeSend > PROBE_INTERVAL) {
+                sendData("\tREG\t")
+                mLastProbeSendTime = currentTime
+            }
+        }
+
+        if(needToBroadcast) Log.d(TAG, "Need To Broadcast")
+        if (mRequiresFrameBroadcast) Log.d(TAG, "Need To Broadcast Frame")
+        if (mRequiresDataBroadcast) Log.d(TAG, "Need To Broadcast Data")
     }
 
     companion object{
